74c7204dc6680d52b5e3052e7f3f13aa89a8ad5b
Charles Anderson <hpcngmoh@gmail.com>
2022-08-03 17:02:55 -0400

    using default mysql function for created on timestamp instead of passing it explicitly

--- src/tools/database/new_egg.ts
+++ src/tools/database/new_egg.ts
@@ -5,6 +5,6 @@ import { ConnectPromise } from "../client/get_db_connection";
  */
 export default function (db: ConnectPromise): (owner_id: string, type: string, p1: string | null, p2: string | null, created: number) => Promise<void> {
     return async (owner_id: string, type: string, p1: string | null = null, p2: string | null = null, created: number = Date.now()): Promise<void> => {
-        await db.query(`INSERT INTO eggs(owner, type, adam, eve, created_on) VALUES("${ owner_id }", "${ type }", "${ p1 }", "${ p2 }", ${ created })`);
+        await db.query(`INSERT INTO eggs(owner, type, adam, eve) VALUES("${ owner_id }", "${ type }", "${ p1 }", "${ p2 }")`);
     };
 }
