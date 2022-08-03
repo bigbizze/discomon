a4a34a525f6553fd55b77f65b2675ffe1248a57d
Charles Anderson <hpcngmoh@gmail.com>
2022-08-03 17:02:31 -0400

    adding additional check for shield

--- src/tools/database/has_shield.ts
+++ src/tools/database/has_shield.ts
@@ -5,6 +5,9 @@ import { ConnectPromise } from "../client/get_db_connection";
 export default function (db: ConnectPromise): (owner_id: string) => Promise<boolean> {
     return async (owner_id: string): Promise<boolean> => {
         const shield = await db.query(`SELECT shield FROM inventory WHERE owner = ${ owner_id }`);
+        if (!shield || shield.length === 0) {
+            return false;
+        }
         return Date.now() - shield[0].shield < 86400000 / 2;
     };
 }
